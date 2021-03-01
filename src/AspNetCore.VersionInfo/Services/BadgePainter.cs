09286ade959efd7259732781950ef013d73f42ee
salem84 <giorgio.lasala@gmail.com>
2021-03-02 00:36:42 +0100

    duplicated colors

--- src/AspNetCore.VersionInfo/Services/BadgePainter.cs
+++ src/AspNetCore.VersionInfo/Services/BadgePainter.cs
@@ -24,9 +24,7 @@ namespace AspNetCore.VersionInfo.Services
         public static string Orange = "#fe7d37";
         public static string Red = "#e05d44";
         public static string Blue = "#007ec6";
-        public static string Grey = "#555";
         public static string Gray = "#555";
-        public static string LightGrey = "#9f9f9f";
         public static string LightGray = "#9f9f9f";
     }
 
