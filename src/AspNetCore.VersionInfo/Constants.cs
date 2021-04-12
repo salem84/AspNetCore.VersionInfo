fc428e4881784e80e88d6ba4a9dee636039f2ef1
salem84 <giorgio.lasala@gmail.com>
2021-04-12 19:36:00 +0200

    added environmentprovider

--- src/AspNetCore.VersionInfo/Constants.cs
+++ src/AspNetCore.VersionInfo/Constants.cs
@@ -17,14 +17,24 @@ namespace AspNetCore.VersionInfo
         public const string DEFAULT_BADGE_ENDPOINT_URL = "/version/badge/{versionInfoId}";
 
         public const string KEY_ENTRY_ASSEMBLY_VERSION = "EntryAssemblyVersion";
-        public const string KEY_RUNTIME_VERSION = "RuntimeVersion";
-        public const string KEY_DOTNET_VERSION = "DotNetVersion";
 
         public const string KEY_RUNTIMEINFORMATION_FRAMEWORKDESCRIPTION = "RuntimeInformation.FrameworkDescription";
         public const string KEY_RUNTIMEINFORMATION_OSDESCRIPTION = "RuntimeInformation.OsDescription";
         public const string KEY_RUNTIMEINFORMATION_OSARCHITECTURE = "RuntimeInformation.OsArchitecture";
         public const string KEY_RUNTIMEINFORMATION_PROCESSARCHITECTURE = "RuntimeInformation.ProcessArchitecture";
         public const string KEY_RUNTIMEINFORMATION_RUNTIMEIDENTIFIER = "RuntimeInformation.RuntimeIdentifier";
+
+        public const string KEY_ENVIRONMENT_UPTIME = "Environment.Uptime";
+        public const string KEY_ENVIRONMENT_OSVERSION = "Environment.OSVersion";
+        public const string KEY_ENVIRONMENT_ISOSWINDOWS = "Environment.IsOsWindows";
+        public const string KEY_ENVIRONMENT_IS64BITOPERATINGSYSTEM = "Environment.Is64BitOperatingSystem";
+        public const string KEY_ENVIRONMENT_IS64BITPROCESS = "Environment.Is64BitProcess";
+        public const string KEY_ENVIRONMENT_PROCESSORCOUNT = "Environment.ProcessorCount";
+        public const string KEY_ENVIRONMENT_MACHINENAME = "Environment.MachineName";
+        public const string KEY_ENVIRONMENT_SYSTEMDIRECTORY = "Environment.SystemDirectory";
+        public const string KEY_ENVIRONMENT_WORKINGDIRECTORY = "Environment.WorkingDirectory";
+        public const string KEY_ENVIRONMENT_COMMANDLINE = "Environment.CommandLine";
+        public const string KEY_ENVIRONMENT_VERSION = "Environment.DotNetVersion";
 
         public const string BADGE_PARAM_VERSIONINFOID = "versionInfoId";
         public const string BADGE_PARAM_LABEL = "label";
