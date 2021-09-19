2a3c7af2c1b1e4bd0966ff19e039160299d1f1b7
salem84 <giorgio.lasala@gmail.com>
2021-09-19 22:34:15 +0200

    provider with environment variables list

---
+++ src/AspNetCore.VersionInfo/Providers/EnvironmentVariablesProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCore.VersionInfo.Providers
+{
+    public class EnvironmentVariablesProvider : IInfoProvider
+    {
+        public string Name => nameof(EnvironmentVariablesProvider);
+
+        public IDictionary<string, string> GetData()
+        {
+            var dict = new Dictionary<string, string>();
+
+            foreach (DictionaryEntry envVar in Environment.GetEnvironmentVariables())
+            {
+                dict.Add(envVar.Key.ToString(), envVar.Value.ToString());
+            }
+
+
+            return dict;
+        }
+    }
+}
