f3badc5e4ea914d7c1969f39d16f2c0a2169a401
salem84 <salem84@users.noreply.github.com>
2022-08-02 23:25:44 +0200

    added badge painter test

---
+++ tests/AspNetCore.VersionInfo.Tests/BadgePainterTest.cs
@@ -0,0 +1,33 @@
+﻿using AspNetCore.VersionInfo.Middleware;
+using AspNetCore.VersionInfo.Services;
+using AspNetCore.VersionInfo.Tests.Mock;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetCore.VersionInfo.Tests
+{
+    public class BadgePainterTest : BaseIocTest
+    {
+        [Fact]
+        public void DrawBadge()
+        {
+            // Arrange
+            var painter = new BadgePainter();
+
+            // Act
+            var badge = painter.Draw("Framework", ".NET 6.0.0", Constants.BADGE_DEFAULT_COLOR, Style.Flat);
+
+            // Assert
+            Assert.StartsWith("<svg", badge);
+        }
+
+    }
+}
