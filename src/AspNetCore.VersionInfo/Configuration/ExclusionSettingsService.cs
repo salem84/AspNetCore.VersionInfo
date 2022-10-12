e84f7f2989cf1d8f732394147f04973ef24ff310
Rebin <4rebin@gmail.com>
2022-10-12 22:16:08 +0300

    Added startup configuration options

---
+++ src/AspNetCore.VersionInfo/Configuration/ExclusionSettingsService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCore.VersionInfo.Configuration
+{
+    public class ExclusionSettingsService : IExclusionSettings
+    {
+        public IEnumerable<string> keys { get; set; }
+        public ExclusionSettingsService(List<String> keys)
+        {
+            this.keys = keys;
+        }
+    }
+}
---
+++ src/AspNetCore.VersionInfo/Configuration/IExclusionSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCore.VersionInfo.Configuration
+{
+    public interface IExclusionSettings
+    {
+        IEnumerable<string> keys { get; set; }
+    }
+}
