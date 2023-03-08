ec8a06c1abca71909b2badb3e76cf42c5c3a2f12
salem84 <salem84@users.noreply.github.com>
2023-03-08 19:05:25 +0100

    authorization sample

---
+++ samples/Authentication/Pages/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCore.VersionInfo.Samples.Authentication.Pages
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
